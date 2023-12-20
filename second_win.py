c9aac78d959de00ecdb6101bc5df56484a108ec2
celynhs <124501812+celynhs@users.noreply.github.com>
2023-12-20 18:59:29 +0700

    Update second_win.py

--- second_win.py
+++ second_win.py
@@ -7,6 +7,13 @@ from instr import *
 from final_win import *
 # tambah disini
 
+class Experiment(): # buat kelas experiment untuk menyimpan data inputan (jawaban user)
+    ___ ___(___, ___, ___, ___, ___): # konstruktor kelas dengan parameter : self, age , test1, test2,test3
+        # setel tiap properti seperti self.properti = properti
+        ___.___ = ___ # setel age 
+        ___.___ = ___ # setel test1 
+        ___.___ = ___ # setel test2 
+        ___.___ = ___ # setel test3
 
 # buat kelas untuk screen kedua     
 class TestWin(QWidget): # kelas turunan QWidget
@@ -21,9 +28,8 @@ class TestWin(QWidget): # kelas turunan QWidget
         self.connects()
         # panggil method untuk menyetel tampilan layar kedua/testing (ukuran , judul layar, dan lokasi)
         self.set_appear()
-        # mulai program (tampilkan layar)
-        self.show()
         # panggil metode ke self untuk munculkan layar
+        self.next_click() # panggil metode ke self untuk munculkan layar
 
     # metode untuk setelan tampilan UI screen kedua (testing screen)
     def initUI(self):
@@ -117,7 +123,71 @@ class TestWin(QWidget): # kelas turunan QWidget
     def connects(self):
         self.button_1.clicked.connect(self.next_click)
         # sambungkan tombol next dengan fungsi next_click untuk transisi ke layar ketiga
+        # sambungkan tombol test1 dengan fungsi timer_test untuk memicu timer test 1
+        ___.___.___.___(___.___)
+        # sambungkan tombol test2 dengan fungsi timer_sits untuk memicu timer test 2
+        ___.___.___.___(___.___)
+        # sambungkan tombol test3 dengan fungsi timer_final untuk memicu timer test 3
+        ___.___.___.___(___.___)
 
+    def timer_test(self): # metode timer untuk test pertama 
+        global time # setel variabel time sebagai global variabel
+        time = ___(_, _, __) # buat objek time dengan kelas QTime(jam,menit,detik) dimana waktu hanya 15 detik
+        ___.timer = ___() # buat objek timer dengan kelas QTimer()
+        ___.___.___.___(___.___) # sambungkan timer dengan fungsi timer1Event (agar timer berjalan dan terupdate)
+        ___.___.___(1000) # mulai timer dengan metode start
+
+
+    def timer_sits(self): # metode timer untuk test kedua
+        global time
+        time = ___(_, _, __) # buat objek time dengan kelas QTime(jam,menit,detik) dimana waktu hanya 30 detik
+        ___.timer = ___() # buat objek timer dengan kelas QTimer()
+        ___.___.___.___(___.___) # sambungkan timer dengan fungsi timer2Event (agar timer berjalan dan terupdate)
+        #one squat in 1.5 seconds
+        ___.___.___(1500) # mulai timer dengan metode start
+
+
+    def timer_final(self): # metode timer untuk test akhir
+        global time
+        time = ___(_, _, __) # buat objek time dengan kelas QTime(jam,menit,detik) dimana waktu hanya 1 menit
+        ___.timer = ___() # buat objek timer dengan kelas QTimer()
+        ___.___.___.___(___.___) # sambungkan timer dengan fungsi timer3Event (agar timer berjalan dan terupdate)
+        ___.___.___(1000) # mulai timer dengan metode start
+
+
+    def timer1Event(self):
+        global time
+        time = ___.___(-1) # gunakan metode addSecs untuk countdown waktu dimana timer berkurang 1 setiap detiknya 
+        ___.___.___(___.___("___:___:___")) # setel text timer (label_6) dengan teks waktu yg diubah menjadi string dalam format "hh:mm:ss"
+        ___.___.___(___("___", __, ___.___)) # setel font dari text timer (label_6) dengan style "Timer" , ukuran 36  dan format bold
+        ___.___.___("color: rgb(0,0,0)") # setel warna tulisan dari label_6 / text timer dengan color: rgb(0,0,0)
+        __ ___.___("___:___:___") == "___:___:___": # jika waktu habis (jika time yang diconvert ke string sama dengan "00:00:00")
+            ___.___.___() # maka stop timer
+
+
+    def timer2Event(self):
+        global time
+        time = ___.___(-1) # gunakan metode addSecs untuk countdown waktu dimana timer berkurang 1 setiap detiknya 
+        ___.___.___(___.___("___:___:___")[6:8]) # setel text timer (label_6) dengan teks waktu yg diubah menjadi string dalam format "hh:mm:ss"
+        ___.___.___(___("___", __, ___.___)) # setel font dari text timer (label_6) dengan style "Timer" , ukuran 36  dan format bold
+        ___.___.___("color: rgb(0,0,0)") # setel warna tulisan dari label_6 / text timer dengan color: rgb(0,0,0)
+        __ ___.___("___:___:___") == "___:___:___": # jika waktu habis (jika time yang diconvert ke string sama dengan "00:00:00")
+            ___.___.___() # maka stop timer
+
+
+    def timer3Event(self):
+        global time
+        time = ___.___(-1) # gunakan metode addSecs untuk countdown waktu dimana timer berkurang 1 setiap detiknya 
+        ___.___.___(___.___("___:___:___")) # setel text timer (label_6) dengan teks waktu yg diubah menjadi string dalam format "hh:mm:ss"
+        if ___(___.___("hh:mm:ss")[6:8]) __ __: # jika time / waktu diatas 45 detik (waktu : format integer dari time yang diconvert ke string dlm forma "hh:mm:ss")
+            ___.___.___("color: rgb(___,___,___)") # maka setel warna tulisan dari text timer / label_6 dengan rgb(0,255,0)
+        elif ___(___.___("hh:mm:ss")[6:8]) __ __: # jika time / waktu dibawah 15 detik (waktu : format integer dari time yang diconvert ke string dlm forma "hh:mm:ss")
+            ___.___.___("color: rgb(___,___,___)") # maka setel warna tulisan dari text timer / label_6 dengan rgb(0,255,0)
+        ___: # selain itu 
+            ___.___.___("color: rgb(___,___,___)") # maka setel warna tulisan dari text timer / label_6 dengan rgb(0,20,0)
+        ___.___.___(___("___", __, ___.___)) # setel font dari text timer (label_6) dengan style "Timer" , ukuran 36  dan format bold
+        __ ___.___("___:___:___") == "___:___:___": # jika waktu habis (jika time yang diconvert ke string sama dengan "00:00:00")
+            ___.___.___() # maka stop timer
 
     #''' metode untuk penyetelan main window (judul , lokasi , ukuran) '''
 
